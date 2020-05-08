ecb3adf5179d2670f1d1a5b6508abc5b7dc10a64
mishubham <sbnd8197@gmail.com>
2020-05-08 10:17:27 +0530

    Responsive widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_test_web_app/responsive_widget/responsive_widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Shubham Portfolio',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -32,39 +34,58 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  List<Widget> appbarAction() {
+    return <Widget>[
+      FlatButton(onPressed: () {}, child: Text("Home")),
+      Container(
+        width: 10,
+      ),
+      FlatButton(onPressed: () {}, child: Text("About")),
+      Container(
+        width: 10,
+      ),
+      FlatButton(onPressed: () {}, child: Text("Contact")),
+      Container(
+        width: 50,
+      )
+    ];
+  }
+
+  Widget myDrawer(BuildContext context) {
+    return ResponsiveWidget.issmallScreen(context)
+        ? Drawer(
+            child: ListView(children: appbarAction()),
+          )
+        : null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   centerTitle: true,
-      //   title: Text(widget.title),
-      // ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-
-            Text(
-              'You have pushed the button this many times:',
-            ),
-
-
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      backgroundColor: Colors.white,
+      drawer: myDrawer(context),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: ResponsiveWidget.islargeScreen(context) ||
+                ResponsiveWidget.ismediumScreen(context)
+            ? Colors.white
+            : Colors.red,
+        centerTitle: true,
+        title: Text(
+          "My Portfolio",
+          style: TextStyle(color: Colors.black),
         ),
+        actions:
+            !ResponsiveWidget.issmallScreen(context) ? appbarAction() : null,
       ),
-      floatingActionButton: FloatingActionButton(
-        heroTag: "myapp",
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      body: ResponsiveWidget.islargeScreen(context) ||
+              ResponsiveWidget.ismediumScreen(context)
+          ? Container(
+              color: Colors.white,
+            )
+          : Container(
+              color: Colors.red,
+            ),
     );
   }
 }
