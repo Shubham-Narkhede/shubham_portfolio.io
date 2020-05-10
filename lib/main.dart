edf3ad374a6f9cda74b2a85a254dc0c7cad55091
mishubham <sbnd8197@gmail.com>
2020-05-10 11:17:11 +0530

    Added files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_test_web_app/responsive_widget/responsive_widget.dart';
+import 'package:my_test_web_app/widgets/pageview_ui.dart';
+
+import 'background_paint/background_paint.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,66 +29,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  List<Widget> appbarAction() {
-    return <Widget>[
-      FlatButton(onPressed: () {}, child: Text("Home")),
-      Container(
-        width: 10,
-      ),
-      FlatButton(onPressed: () {}, child: Text("About")),
-      Container(
-        width: 10,
-      ),
-      FlatButton(onPressed: () {}, child: Text("Contact")),
-      Container(
-        width: 50,
-      )
-    ];
-  }
-
-  Widget myDrawer(BuildContext context) {
-    return ResponsiveWidget.issmallScreen(context)
-        ? Drawer(
-            child: ListView(children: appbarAction()),
-          )
-        : null;
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      drawer: myDrawer(context),
-      appBar: AppBar(
-        elevation: 0.0,
-        backgroundColor: ResponsiveWidget.islargeScreen(context) ||
-                ResponsiveWidget.ismediumScreen(context)
-            ? Colors.white
-            : Colors.red,
-        centerTitle: true,
-        title: Text(
-          "My Portfolio",
-          style: TextStyle(color: Colors.black),
-        ),
-        actions:
-            !ResponsiveWidget.issmallScreen(context) ? appbarAction() : null,
-      ),
-      body: ResponsiveWidget.islargeScreen(context) ||
-              ResponsiveWidget.ismediumScreen(context)
-          ? Container(
-              color: Colors.white,
-            )
-          : Container(
-              color: Colors.red,
-            ),
+    return ResponsiveWidget(
+      largeScreen: Scaffold(
+          body:  MainPage(initialPage: 0),),
     );
   }
 }
